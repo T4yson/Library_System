d31a44d8d9b61a56a5a926b7fe3dd5db7816cbd4
Igor Tayson Bresolin Savero <igor_savero@estudante.sesisenai.org.br>
2026-02-26 19:47:42 -0300

    refactory: ajusta classe UsuarioService e implementa DTO

--- Sistema-Biblioteca/src/main/java/com/exemplo/biblioteca/service/UsuarioService.java
+++ Sistema-Biblioteca/src/main/java/com/exemplo/biblioteca/service/UsuarioService.java
@@ -1,62 +1,97 @@
 package com.exemplo.biblioteca.service;
 
-import com.exemplo.biblioteca.Conexao;
+import com.exemplo.biblioteca.dto.Usuario.UsuarioRequisicaoDTO;
+import com.exemplo.biblioteca.dto.Usuario.UsuarioRespostaDTO;
+import com.exemplo.biblioteca.model.Livro;
+import com.exemplo.biblioteca.utils.Conexao;
 import com.exemplo.biblioteca.dao.UsuarioDAO;
-import com.exemplo.biblioteca.model.Emprestimo;
 import com.exemplo.biblioteca.model.Usuario;
+import org.springframework.stereotype.Service;
 
-import javax.sql.rowset.CachedRowSet;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UsuarioService {
 
     UsuarioDAO usuarioDAO = new UsuarioDAO();
 
-    public Usuario criarUsuario(Usuario usuario) {
-        if (usuario.getNome() == null || usuario.getNome().trim().isEmpty()) {
-            throw new RuntimeException("O nome de Usuário é obrigatório!");
-        }
+    public UsuarioRespostaDTO criarUsuario(UsuarioRespostaDTO dto) {
 
-        if (usuario.getEmail() == null || usuario.getEmail().trim().isEmpty()) {
-            throw new RuntimeException("O email de usuário é obrigatório!");
-        }
+        try (Connection conn = Conexao.connection()){
 
-        try (Connection conn = Conexao.connection()) {
+            Usuario usuario = new Usuario();
+            usuario.setNome(dto.nome());
+            usuario.setEmail(dto.email());
+
             usuarioDAO.criarUsuario(conn, usuario);
-            System.out.println("Usuário cadastrado com sucesso!");
-        } catch (SQLException e) {
-            throw new RuntimeException("Erro ao cadastrar usuário", e);
-        }
-        return usuario;
-    }
 
-    public Usuario buscarPorId(Long id) {
-        try (Connection conn = Conexao.connection()){
-            return usuarioDAO.buscarPorId(conn, id);
-        }catch (SQLException e) {
-            throw new RuntimeException("Erro ao buscar usuário!", e);
-        }
-    }
+            return new UsuarioRespostaDTO(usuario.getId(), usuario.getNome(), usuario.getEmail());
 
-    public List<Usuario> buscarTodos() {
-        try (Connection conn = Conexao.connection()){
-            return usuarioDAO.buscarTodos(conn);
         }catch (SQLException e) {
-            throw new RuntimeException("Erro ao listar", e);
+            throw new RuntimeException("Erro ao cadastrar usuario: " + e.getMessage(), e);
         }
     }
-    public void atualizar (Usuario usuario) {
-        if (usuario.getId() == null) {
-            throw new RuntimeException("O ID do usuário é obrigatório!");
-        }
+    public List<UsuarioRespostaDTO> buscarTodos() {
         try (Connection conn = Conexao.connection()) {
-            usuarioDAO.atualizar(conn, usuario);
-            System.out.println("Usuário atualizado com sucesso!");
+            List<Usuario> usuarios = usuarioDAO.buscarTodos(conn);
+            List<UsuarioRespostaDTO> lista = new ArrayList<>();
+
+            for (Usuario usuario : usuarios) {
+                lista.add(new UsuarioRespostaDTO(
+                        usuario.getId(),
+                        usuario.getNome(),
+                        usuario.getEmail()
+                ));
+            }
+            return lista;
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao listar usuário" + e.getMessage(), e);
+        }
+    }
+    public UsuarioRespostaDTO buscarPorId(Long id) {
+        try (Connection conn = Conexao.connection()) {
+                Usuario usuario = usuarioDAO.buscarPorId(conn, id);
+
+                if (usuario == null) {
+                    return null;
+                }
+                return new UsuarioRespostaDTO(usuario.getId(), usuario.getNome(), usuario.getEmail());
         }catch (SQLException e) {
-            throw new RuntimeException("Erro ao atualizar usuário", e);
+            throw new RuntimeException("Erro ao buscar por Id" + e.getMessage(), e);
+        }
+    }
+
+    public UsuarioRespostaDTO atualizar (Long id, UsuarioRequisicaoDTO dto) {
+        try (Connection conn = Conexao.connection()) {
+            Usuario usuarioExistente = usuarioDAO.buscarPorId(conn, id);
+
+            if (usuarioExistente == null) {
+                throw new RuntimeException("Usuário não encontrado!");
+            }
+
+            usuarioExistente.setNome(dto.nome());
+            usuarioExistente.setEmail(dto.email());
+
+            usuarioDAO.atualizar(conn, usuarioExistente);
+
+            return new UsuarioRespostaDTO(
+                    usuarioExistente.getId(),
+                    usuarioExistente.getNome(),
+                    usuarioExistente.getEmail()
+            );
+        }catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar usuário" + e.getMessage(), e);
+        }
+    }
+
+    public void deletar(Long id) {
+        try (Connection con = Conexao.connection()) {
+            usuarioDAO.deletar(con, id);
+        }catch (SQLException e) {
+            throw new RuntimeException("Erro ao deletar" + e.getMessage(), e);
         }
     }
 }
